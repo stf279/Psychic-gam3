1f32a02aaf11e6f5d287a4f1cbe8c456a79169b8
stf279 <stf279@yahoo.com>
2018-02-27 18:11:40 -0600

    added p tags

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+let guess = Math.floor(Math.random() * 26);
+
+let myGuess = alpha [guess];
+
+
+
+console.log(guess);
+console.log(myGuess);
